64ffc0649d99cf0d0caaa95be8a730ccc629a9e7
IrinaStefanescu <49494590+IrinaStefanescu@users.noreply.github.com>
2022-01-26 13:45:36 +0200

    added routes in app.module and app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'category-details', component: CategoryDetailsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'terms', component: TermsConditionsComponent },
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ApiService } from './services/api.service';
+import { NotifierModule } from 'angular-notifier';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
+import { ContactComponent } from './contact/contact.component';
+import { CategoryService } from './services/category.service';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TermsConditionsComponent,
+    ContactComponent,
+    CategoryDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NotifierModule
   ],
-  providers: [],
+  providers: [ApiService, CategoryService, ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
